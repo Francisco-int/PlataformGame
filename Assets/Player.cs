21ff9638d0feb2507c12cc2e78f0d46a7e5b905e
Francisco-int <franciscos1205@gmail.com>
2024-04-19 10:59:27 -0300

    0.1

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public float speed;
+    public float jumpForce;
+    [SerializeField] bool jumpAble;
+    [SerializeField] Rigidbody body;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        body.AddForce(new Vector3(horizontal, 0, vertical) * speed);
+
+        if(Input.GetKeyDown(KeyCode.Escape) && jumpAble)
+        {
+            body.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Plataform"))
+        {
+            jumpAble = false;
+        }
+        else
+        {
+            jumpAble = true;
+        }
     }
 }
