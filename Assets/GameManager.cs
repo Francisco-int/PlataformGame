52a769b81c541bf406c54c3d20d2ba40de20741d
Francisco-int <franciscos1205@gmail.com>
2024-04-24 15:20:08 -0300

    0.3

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,16 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlataformsControls : MonoBehaviour
-{
-     void Update()
-    {
-        
-    }
-}
 
 public class GameManager : MonoBehaviour
 {
+    enum {explotion, jump
+
     // Start is called before the first frame update
     void Start()
     {
